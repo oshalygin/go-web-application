7ea2679dd3251ff674c7387781b9c08badeafac4
oshalygin <oshalygin@gmail.com>
2017-06-28 21:17:15 -0700

    Simple web server - static content

--- main.go
+++ main.go
@@ -5,12 +5,21 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	const PORT = ":8080"
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		filePath := "public" + r.URL.Path
+		var filePath string
+		var contentType string
+
+		if r.URL.Path == "/" {
+			filePath = "public/index.html"
+		} else {
+			filePath = "public" + r.URL.Path
+		}
+
 		file, err := os.Open(filePath)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
@@ -18,6 +27,16 @@ func main() {
 		}
 
 		defer file.Close()
+
+		switch {
+		case strings.HasSuffix(r.URL.Path, "css"):
+			contentType = "text/css"
+		case strings.HasSuffix(r.URL.Path, "html"):
+			contentType = "text/html"
+		case strings.HasSuffix(r.URL.Path, "ico"):
+			contentType = "image/ico"
+		}
+		w.Header().Add("Content-Type", contentType)
 		io.Copy(w, file)
 	})
 
