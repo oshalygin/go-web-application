c4ca71242fbab249ac417d3eed74853f58bf534f
oshalygin <oshalygin@gmail.com>
2017-06-28 21:21:34 -0700

    Refactor using FileServer

--- main.go
+++ main.go
@@ -1,46 +1,13 @@
 package main
 
 import (
-	"io"
-	"log"
 	"net/http"
-	"os"
-	"strings"
 )
 
 func main() {
 	const PORT = ":8080"
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		var filePath string
-		var contentType string
-
-		if r.URL.Path == "/" {
-			filePath = "public/index.html"
-		} else {
-			filePath = "public" + r.URL.Path
-		}
-
-		file, err := os.Open(filePath)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			log.Println(err)
-		}
-
-		defer file.Close()
-
-		switch {
-		case strings.HasSuffix(r.URL.Path, "css"):
-			contentType = "text/css"
-		case strings.HasSuffix(r.URL.Path, "html"):
-			contentType = "text/html"
-		case strings.HasSuffix(r.URL.Path, "ico"):
-			contentType = "image/ico"
-		default:
-			contentType = "text/plain"
-		}
-
-		w.Header().Add("Content-Type", contentType)
-		io.Copy(w, file)
+		http.ServeFile(w, r, "public"+r.URL.Path)
 	})
 
 	http.ListenAndServe(PORT, nil)
