608f9d8b19cd4aeedb885487b5ceb3df40b1143c
oshalygin <oshalygin@gmail.com>
2017-07-01 19:16:18 -0700

    Wiring up templates

--- main.go
+++ main.go
@@ -3,20 +3,39 @@ package main
 import (
 	"html/template"
 	"log"
-	"os"
+	"net/http"
 )
 
 func main() {
-	templateString := `Foobar template`
-	t, err := template.New("title").Parse(templateString)
+	const PORT string = ":8080"
 
-	if err != nil {
-		log.Println(err)
-	}
+	templates := hydrateTemplates()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		requestedFile := r.URL.Path[1:]
+		t := templates.Lookup(requestedFile + ".html")
+		if t != nil {
+			err := t.Execute(w, nil)
+			if err != nil {
+				log.Println(err)
+			}
 
-	err = t.Execute(os.Stdout, nil)
-	if err != nil {
-		log.Println(err)
-	}
+		} else {
+			println("something went wrong")
+			w.WriteHeader(http.StatusNotFound)
+		}
+	})
 
+	http.Handle("/img/", http.FileServer(http.Dir("public")))
+	http.Handle("/css/", http.FileServer(http.Dir("public")))
+
+	http.ListenAndServe(PORT, nil)
+
+}
+
+func hydrateTemplates() *template.Template {
+	result := template.New("templates")
+	const basePath string = "templates"
+
+	template.Must(result.ParseGlob(basePath + "/*.html"))
+	return result
 }
